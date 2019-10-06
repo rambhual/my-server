e419afbd0254d4f8fb92410be0a3a5c81b2d2ebc
unknown <Rampritsahani@outlook.in>
2019-10-06 12:59:55 +0530

    create orders

---
+++ api/orders/controllers/Orders.js
@@ -0,0 +1,35 @@
+"use strict";
+const stripe = require("stripe")("sk_test_9ioWnXnr0tAUf3Z9hDElPFR5");
+
+module.exports = {
+  create: async ctx => {
+    const {
+      address,
+      amount,
+      brews,
+      postalCode,
+      token,
+      city
+    } = ctx.request.body;
+
+    // Send charge to Stripe
+    const charge = await stripe.charges.create({
+      amount: amount * 100,
+      currency: "usd",
+      description: `Order ${new Date(Date.now())} - User ${ctx.state.user._id}`,
+      source: token
+    });
+
+    // Create order in database
+    const order = await strapi.services.orders.add({
+      user: ctx.state.user._id,
+      address,
+      amount,
+      brews,
+      postalCode,
+      city
+    });
+
+    return order;
+  }
+};
